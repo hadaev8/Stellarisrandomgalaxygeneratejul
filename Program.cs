c487c949a960d45d25605efe13dfcd8afb0d82cc
hadaev8 <had-95@yandex.ru>
2017-03-06 15:18:41 +0300

    1.02

--- Program.cs
+++ Program.cs
@@ -22,10 +22,8 @@ namespace galgen
                 list[n] = value;
             }
         }
-        public static void GenJulSet(List<Tuple<int, int>> vect, int w, int h, int maxIter, double zoom, Random rnd, ref int randh, ref int randw, ref Complex c)
+        public static void getvaluesforjuliaset (int w, int h, Random rnd, ref int randh, ref int randw, ref Complex c)
         {
-            startofcycle:
-            vect.Clear();
             // рандомим произвольную постоянную
             double c1 = rnd.NextDouble();
             double c2 = rnd.NextDouble();
@@ -38,96 +36,125 @@ namespace galgen
                 c2 = c2 * (-1);
             }
             c = new Complex(c1, c2);
+            // рандомный сдвиг, если нужен
+            randw = rnd.Next(-w / 2, w / 2);
+            randh = rnd.Next(-h / 2, h / 2);
+        }
+        public static List<Tuple<int, int>> GenJulSet(int w, int h, int maxIter, double zoom, Random rnd, int randh, int randw, Complex c)
+        {
+            List<Tuple<int, int>> vect = new List<Tuple<int, int>>();
+            
+
             // вспомогательные переменные для приведения координат
             double r = 0.5 * (1 + Math.Sqrt(1 + 4 * Complex.Abs(c)));
             double xStep = 2 * r / w;
             double yStep = 2 * r / h;
-            // рандомный сдвиг, если нужен
-            randw = rnd.Next(-w / 2, w / 2);
-            randh = rnd.Next(-h / 2, h / 2);
-            //randw = 0;
-            //randh = 0;
+            double yhelp = 2 * r * (1 - zoom);
+
+            double yfirst = h - h * zoom + randh;
+            double xfirst = w - w * zoom + randw;
+            double ylast = h * zoom + randh;
+            double xlast = w * zoom + randw;
+
             // чтобы убивать повторяющиеся
             double predx = 0;
             int povtor = 0;
+
             bool eptline = true;
-            for (double y = h - h * zoom + randh; y < h * zoom + randh; y++)
+            // первая строка по иксу (y = 0)
+            Parallel.For(0, 99, (index, loopState) =>
+            //for (double x = xfirst; x < xlast; x++)
             {
-                for (double x = w - w * zoom + randw; x < w * zoom + randw; x++)
+                int i = maxIter;
+                Complex z = new Complex(-r + (xfirst + index) * xStep, -r + yfirst * yStep);
+                while (Complex.Abs(z) < r && i != 0)
                 {
-                    int i = maxIter;
-                    Complex z = new Complex(-r + x * xStep, -r + y * yStep);
-                    while (Complex.Abs(z) < r && i != 0)
-                    {
-                        z = (z * z + c);
-                        --i;
-                    }
-                    if (i == 0)
-                        eptline = false;
+                    z = (z * z + c);
+                    --i;
                 }
-                if (eptline)
-                    goto startofcycle;
-
+                if (i == 0)
+                {
+                    eptline = false;
+                    loopState.Stop();
+                }
+            });
+            if (eptline == false)
+            {
+                goto startofcycle;
             }
-            for (double x = h - h * zoom + randw; x < h * zoom + randw; x++)
+            // последняя строка по иксу (y = 99)
+            eptline = true;
+            //for (double x = xfirst; x < xlast; x++)
+            Parallel.For(0, 99, (index, loopState) =>
             {
-                for (double y = w - w * zoom + randh; y < w * zoom + randh; y++)
+                int i = maxIter;
+                Complex z = new Complex(-r + (xfirst + index) * xStep, -r + ylast * yStep);
+                while (Complex.Abs(z) < r && i != 0)
                 {
-                    int i = maxIter;
-                    Complex z = new Complex(-r + x * xStep, -r + y * yStep);
-                    while (Complex.Abs(z) < r && i != 0)
-                    {
-                        z = (z * z + c);
-                        --i;
-                    }
-                    if (i == 0)
-                        eptline = false;
+                    z = (z * z + c);
+                    --i;
                 }
-                if (eptline)
-                    goto startofcycle;
-
+                if (i == 0)
+                {
+                    eptline = false;
+                    loopState.Stop();
+                }
+            });
+            if (eptline == false)
+            {
+                goto startofcycle;
             }
-            for (double y = h * zoom + randh; y > h - h * zoom + randh; y--)
+            // первая строка по игреку (x = 0)
+            eptline = true;
+            //for (double y = yfirst; y < ylast; y++)
+            Parallel.For(0, 99, (index, loopState) =>
             {
-                for (double x = w - w * zoom + randw; x < w * zoom + randw; x++)
+                int i = maxIter;
+                Complex z = new Complex(-r + xfirst * xStep, -r + (yfirst + index) * yStep);
+                while (Complex.Abs(z) < r && i != 0)
                 {
-                    int i = maxIter;
-                    Complex z = new Complex(-r + x * xStep, -r + y * yStep);
-                    while (Complex.Abs(z) < r && i != 0)
-                    {
-                        z = (z * z + c);
-                        --i;
-                    }
-                    if (i == 0)
-                        eptline = false;
+                    z = (z * z + c);
+                    --i;
                 }
-                if (eptline)
-                    goto startofcycle;
-
+                if (i == 0)
+                {
+                    eptline = false;
+                    loopState.Stop();
+                }
+            });
+            if (eptline == false)
+            {
+                goto startofcycle;
             }
-            for (double x = h * zoom + randw; x > h - h * zoom + randw; x--)
+            // последняя строка по игреку (x = 99)
+            eptline = true;
+            //for (double y = yfirst; y < ylast; y++)
+            Parallel.For(0, 99, (index, loopState) =>
             {
-                for (double y = w - w * zoom + randh; y < w * zoom + randh; y++)
+                int i = maxIter;
+                Complex z = new Complex(-r + xlast * xStep, -r + (yfirst + index) * yStep);
+                while (Complex.Abs(z) < r && i != 0)
                 {
-                    int i = maxIter;
-                    Complex z = new Complex(-r + x * xStep, -r + y * yStep);
-                    while (Complex.Abs(z) < r && i != 0)
-                    {
-                        z = (z * z + c);
-                        --i;
-                    }
-                    if (i == 0)
-                        eptline = false;
+                    z = (z * z + c);
+                    --i;
                 }
-                if (eptline)
-                    goto startofcycle;
-
+                if (i == 0)
+                {
+                    eptline = false;
+                    loopState.Stop();
+                }
+            });
+            if (eptline == false)
+            {
+                goto startofcycle;
             }
             int yy = 0;
-            for (double y = h - h * zoom + randh; y < h * zoom + randh; y++)
+
+            //Parallel.For(0, 99, (index, loopState) =>
+            for (double y = yfirst; y < ylast; y++)
             {
                 int xx = 0;
-                for (double x = w - w * zoom + randw; x < w * zoom + randw; x++)
+                for (double x = xfirst; x < xlast; x++)
                 {
                     int i = maxIter;
                     Complex z = new Complex(-r + x * xStep, -r + y * yStep);
@@ -151,21 +178,25 @@ namespace galgen
                         if (povtor == 0)
                         {
                             vect.Add(new Tuple<int, int>(xx, yy));
+                            //try
+                            //{
+                            //    vect.Add(new Tuple<int, int>(xx, yy));
+                            //}
+                            //catch (Exception e)
+                            //{
+                            //    Console.WriteLine(e);
+                            //}
                         }
                     }
                     xx++;
                 }
                 yy++;
                 if (vect.Count > 3000)
+                    //loopState.Stop();
                     goto startofcycle;
-            }
-            // отсекаем слишком полные и слишком пустые
-            if (vect.Count < 50)
-                goto startofcycle;
-            //Console.WriteLine(vect.Count);
-            //exporttoconsole(vect);
-            //if (Console.ReadKey().Key == ConsoleKey.Enter)
-            //    goto startofcycle;
+            }//);
+            startofcycle:
+            return vect;
         }
         public static void exporttoconsole(List<Tuple<int, int>> vect)
         {
@@ -185,7 +216,7 @@ namespace galgen
 
             image.Save(Way_out_pic);
         }
-        public static void exporttofile(List<Tuple<int, int>> vect, int w, int h, int maxIter, double zoom, Random rnd, Tuple<int, int, int, Complex> currentlog, string filename, string Way_out_file)
+        public static void exporttofile(List<Tuple<int, int>> vect, int w, int h, int maxIter, double zoom, Random rnd, Tuple<int, int, int, int, double, double> currentlog, string filename, string Way_out_file)
         {
             using (StreamWriter sw = new StreamWriter(Way_out_file))
             {
@@ -193,9 +224,9 @@ namespace galgen
                 sw.Write("static_galaxy_scenario = {\n\tname = \"" + filename.Replace(".txt", "") + " stars: " + (vect.Count + 1) + "\"\n\tpriority = 0\n\tdefault = no\n\tcolonizable_planet_odds = 1.0\n\tnum_empires = { min = 0 max = 60 }\n\tnum_empire_default = 21\n\tfallen_empire_default = 4\n\tfallen_empire_max = 4\n\tadvanced_empire_default = 7\n\tcore_radius = 0\n\trandom_hyperlanes = yes\n\n");
                 foreach (Tuple<int, int> currenttuple in vect)
                 {
-                    int rand = rnd.Next(-5, 6);
+                    int rand = rnd.Next(-2, 3);
                     int x = (10 * (currenttuple.Item1 - 50) + rand);
-                    rand = rnd.Next(-5, 6);
+                    rand = rnd.Next(-2, 3);
                     int y = (10 * (currenttuple.Item2 - 50) + rand);
                     if (x > 500)
                         x -= 8;
@@ -213,15 +244,12 @@ namespace galgen
                     }
                 }
                 sw.Write("\tsystem = {\n\t\tid = " + (vect.Count + 1) + "\n\t\tposition = {\n\t\tx = 0\n\t\ty = 0\n\t\t}\n\t}\r");
-                sw.Write("}\n#h = " + h + "\n#w = " + w + "\n#c = " + currentlog.Item4 + "\n#zoom = " + zoom + "\n#iter = " + maxIter + "\n#randh = " + currentlog.Item2 + "\n#randw = " + currentlog.Item3);
+                sw.Write("}\n#h = " + h + "\n#w = " + w + "\n#c1 = " + currentlog.Item5 + "\n#c2 = " + currentlog.Item6 + "\n#zoom = " + zoom + "\n#iter = " + maxIter + "\n#randh = " + currentlog.Item3 + "\n#randw = " + currentlog.Item4);
             }
-            Console.WriteLine(filename);
         }
 
         public static void Main(string[] args)
         {
-            //    Console.WindowHeight = 50;
-            //    Console.WindowWidth = 150;
             Console.WindowHeight = Console.LargestWindowHeight;
             Console.WindowWidth = Console.LargestWindowWidth;
             string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "text generation");
@@ -242,15 +270,13 @@ namespace galgen
             //Directory.CreateDirectory(picdirectory);
             Directory.CreateDirectory(picdirectorygood);
             Directory.CreateDirectory(picdirectorybad);
-            var rnd = new Random(726568);
+            Random rnd = new Random();
             // set parameters
             int w = 10000;
             int h = 10000;
             int maxIter = 350;
             double zoom = 0.505;
-
-            //double zoomkoorh = Math.Abs(h - 2 * h * zoom);
-            //double zoomkoorw = Math.Abs(w - 2 * w * zoom);
+            
             // other values
             int randw = 0;
             int randh = 0;
@@ -261,57 +287,61 @@ namespace galgen
             st.Start();
 
             List<List<Tuple<int, int>>> maps = new List<List<Tuple<int, int>>>();
-            List<Tuple<int, int, int, Complex>> log = new List<Tuple<int, int, int, Complex>>();
-            int startvalue = 400;
-            Parallel.For(startvalue, startvalue + 201, index =>
-            //for (int index = startvalue; index < startvalue + 201; index++)
+            List<Tuple<int, int, int, int, double, double>> log = new List<Tuple<int, int, int, int, double, double>>();
+            int startvalue = 1600;
+            //Parallel.For(startvalue, startvalue + 21, index =>
+            for (int index = startvalue; index < startvalue + 21; index++)
             {
-                List<Tuple<int, int>> vect = new List<Tuple<int, int>>();
-                GenJulSet(vect, w, h, maxIter, zoom, rnd, ref randh, ref randw, ref c);
+                // generate julia set
+                startofcycle:
+                getvaluesforjuliaset(w, h, rnd, ref randh, ref randw, ref c);
+                List<Tuple<int, int>> vect = GenJulSet(w, h, maxIter, zoom, rnd, randh, randw, c);
+                if (vect.Count > 3000 || vect.Count < 50)
+                    goto startofcycle;
+
                 maps.Add(vect);
-                log.Add(new Tuple<int, int, int, Complex>(0, randh, randw, c));
+                
+                log.Add(new Tuple<int, int, int, int, double, double>((maps.Count - 1), 0, randh, randw, c.Real, c.Imaginary));
                 Console.WriteLine(index);
-            });
+            }//);
             Console.WriteLine(st.Elapsed);
+            Console.WriteLine(maps.Count);
             foreach (List<Tuple<int, int>> vect in maps)
-            //for (int index = 400; index < 601; index++)
             {
                 // file name
                 string filename = "Insane Julia Set Rand NS " + (maps.IndexOf(vect) + startvalue);
                 
-                // generate julia set
-                //GenJulSet(vect, w, h, maxIter, zoom, rnd, ref randh, ref randw, ref c);
-
                 // export to console
-                exporttoconsole(vect);
+                //exporttoconsole(vect);
 
                 Way_out_pic = Path.Combine(picdirectorybad, filename + ".jpg");
-                
-                if (Console.ReadKey().Key == ConsoleKey.Enter)
-                {
-                    Way_out_pic = Path.Combine(picdirectorygood, filename + ".jpg");
 
-                    // лучше я не придумал для замены 1 значения тупла
-                    Tuple<int, int, int, Complex> currentlog = log[maps.IndexOf(vect)];
-                    currentlog = new Tuple<int, int, int, Complex>(1, currentlog.Item2, currentlog.Item3, currentlog.Item4);
-                    log.RemoveAt(maps.IndexOf(vect));
-                    log.Insert(maps.IndexOf(vect), currentlog);
+                //if (Console.ReadKey().Key == ConsoleKey.Enter)
+                //{
+                //    Way_out_pic = Path.Combine(picdirectorygood, filename + ".jpg");
 
-                    // вывод в файл, если удачно
-                    exporttofile(vect, w, h, maxIter, zoom, rnd, log[maps.IndexOf(vect)], filename, Path.Combine(newdirectory, filename + ".txt"));
-                }
+                //    // лучше я не придумал для замены 1 значения тупла
+                //    Tuple<int, int, int, int, double, double> currentlog = log[maps.IndexOf(vect)];
+                //    currentlog = new Tuple<int, int, int, int, double, double>(1, currentlog.Item2, currentlog.Item3, currentlog.Item4, currentlog.Item5, currentlog.Item6);
+                //    log.RemoveAt(maps.IndexOf(vect));
+                //    log.Insert(maps.IndexOf(vect), currentlog);
 
-                Console.Clear();
+                //}
+
+                //Console.Clear();
+
+                // вывод в файл
+                exporttofile(vect, w, h, maxIter, zoom, rnd, log[maps.IndexOf(vect)], filename, Path.Combine(newdirectory, filename + ".txt"));
 
                 // export to pic
                 exporttopic(vect, Way_out_pic);
                 
-            }//);
-            using (StreamWriter sw = new StreamWriter(Path.Combine(directory, "log.txt")))
-            {
-                foreach (Tuple<int, int, int, Complex> currenttuple in log)
-                    sw.Write(currenttuple + "\n");
             }
+            //using (StreamWriter sw = new StreamWriter(Path.Combine(directory, "log.txt")))
+            //{
+            //    foreach (Tuple<int, int, int, int, double, double> currenttuple in log)
+            //        sw.Write(currenttuple + "\n");
+            //}
             st.Stop();
             Console.WriteLine(st.Elapsed);
         }
