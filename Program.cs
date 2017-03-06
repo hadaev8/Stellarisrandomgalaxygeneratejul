714993258018910b88c3ddc3c183d87b5d11dec5
Lanrik <Lanrik@users.noreply.github.com>
2017-03-06 17:13:01 +0400

    refactored

--- Program.cs
+++ Program.cs
@@ -22,8 +22,17 @@ namespace galgen
                 list[n] = value;
             }
         }
-        public static void getvaluesforjuliaset (int w, int h, Random rnd, ref int randh, ref int randw, ref Complex c)
+
+        public static List<Point> GenJulSet(int w, int h, int maxIter, double zoom, int randomSeed)
         {
+            List<Point> vect = new List<Point>();
+
+            Random rnd;
+            if (randomSeed == 0)
+                rnd = new Random();
+            else
+                rnd = new Random(randomSeed);
+
             // рандомим произвольную постоянную
             double c1 = rnd.NextDouble();
             double c2 = rnd.NextDouble();
@@ -35,15 +44,10 @@ namespace galgen
             {
                 c2 = c2 * (-1);
             }
-            c = new Complex(c1, c2);
+            Complex c = new Complex(c1, c2);
             // рандомный сдвиг, если нужен
-            randw = rnd.Next(-w / 2, w / 2);
-            randh = rnd.Next(-h / 2, h / 2);
-        }
-        public static List<Tuple<int, int>> GenJulSet(int w, int h, int maxIter, double zoom, Random rnd, int randh, int randw, Complex c)
-        {
-            List<Tuple<int, int>> vect = new List<Tuple<int, int>>();
-            
+            int randw = rnd.Next(-w / 2, w / 2);
+            int randh = rnd.Next(-h / 2, h / 2);
 
             // вспомогательные переменные для приведения координат
             double r = 0.5 * (1 + Math.Sqrt(1 + 4 * Complex.Abs(c)));
@@ -177,7 +181,7 @@ namespace galgen
                         predx = x;
                         if (povtor == 0)
                         {
-                            vect.Add(new Tuple<int, int>(xx, yy));
+                            vect.Add(new Point(xx, yy));
                             //try
                             //{
                             //    vect.Add(new Tuple<int, int>(xx, yy));
@@ -195,39 +199,39 @@ namespace galgen
                     //loopState.Stop();
                     goto startofcycle;
             }//);
-            startofcycle:
+        startofcycle:
             return vect;
         }
-        public static void exporttoconsole(List<Tuple<int, int>> vect)
+        public static void exporttoconsole(List<Point> vect)
         {
             Console.Clear();
-            foreach (Tuple<int, int> currenttuple in vect)
+            foreach (Point currenttuple in vect)
             {
-                Console.SetCursorPosition(currenttuple.Item1, currenttuple.Item2);
+                Console.SetCursorPosition(currenttuple.x, currenttuple.y);
                 Console.Write('#');
             }
             Console.WriteLine();
         }
-        public static void exporttopic(List<Tuple<int, int>> vect, string Way_out_pic)
+        public static void exporttopic(List<Point> vect, string Way_out_pic)
         {
             Bitmap image = new Bitmap(100, 100);
-            foreach (Tuple<int, int> currenttuple in vect)
-                image.SetPixel(currenttuple.Item1, currenttuple.Item2, Color.Black);
+            foreach (Point currenttuple in vect)
+                image.SetPixel(currenttuple.x, currenttuple.y, Color.Black);
 
             image.Save(Way_out_pic);
         }
-        public static void exporttofile(List<Tuple<int, int>> vect, int w, int h, int maxIter, double zoom, Random rnd, Tuple<int, int, int, int, double, double> currentlog, string filename, string Way_out_file)
+        public static void exporttofile(List<Point> vect, int w, int h, int maxIter, double zoom, Random rnd, Log currentlog, string filename, string Way_out_file)
         {
             using (StreamWriter sw = new StreamWriter(Way_out_file))
             {
                 Shuffle(vect, rnd);
                 sw.Write("static_galaxy_scenario = {\n\tname = \"" + filename.Replace(".txt", "") + " stars: " + (vect.Count + 1) + "\"\n\tpriority = 0\n\tdefault = no\n\tcolonizable_planet_odds = 1.0\n\tnum_empires = { min = 0 max = 60 }\n\tnum_empire_default = 21\n\tfallen_empire_default = 4\n\tfallen_empire_max = 4\n\tadvanced_empire_default = 7\n\tcore_radius = 0\n\trandom_hyperlanes = yes\n\n");
-                foreach (Tuple<int, int> currenttuple in vect)
+                foreach (Point currenttuple in vect)
                 {
                     int rand = rnd.Next(-2, 3);
-                    int x = (10 * (currenttuple.Item1 - 50) + rand);
+                    int x = (10 * (currenttuple.x - 50) + rand);
                     rand = rnd.Next(-2, 3);
-                    int y = (10 * (currenttuple.Item2 - 50) + rand);
+                    int y = (10 * (currenttuple.y - 50) + rand);
                     if (x > 500)
                         x -= 8;
                     else if (x < -500)
@@ -244,7 +248,7 @@ namespace galgen
                     }
                 }
                 sw.Write("\tsystem = {\n\t\tid = " + (vect.Count + 1) + "\n\t\tposition = {\n\t\tx = 0\n\t\ty = 0\n\t\t}\n\t}\r");
-                sw.Write("}\n#h = " + h + "\n#w = " + w + "\n#c1 = " + currentlog.Item5 + "\n#c2 = " + currentlog.Item6 + "\n#zoom = " + zoom + "\n#iter = " + maxIter + "\n#randh = " + currentlog.Item3 + "\n#randw = " + currentlog.Item4);
+                sw.Write("}\n#h = " + h + "\n#w = " + w + "\n#c1 = " + currentlog.c1 + "\n#c2 = " + currentlog.c2 + "\n#zoom = " + zoom + "\n#iter = " + maxIter + "\n#randh = " + currentlog.randh + "\n#randw = " + currentlog.randw);
             }
         }
 
@@ -253,30 +257,28 @@ namespace galgen
             Console.WindowHeight = Console.LargestWindowHeight;
             Console.WindowWidth = Console.LargestWindowWidth;
             string directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "text generation");
-            string newdirectory = Path.Combine(directory, "map_new");
-            string picdirectory = Path.Combine(directory, "map_pics");
-            string picdirectorygood = Path.Combine(directory, "good_pics");
-            string picdirectorybad = Path.Combine(directory, "bad_pics");
+            Dictionary<string, string> dirs = new Dictionary<string, string>()
+            {
+                { "map_new",  Path.Combine(directory, "map_new") },
+                { "map_pics",  Path.Combine(directory, "map_pics") },
+                { "good_pics",  Path.Combine(directory, "good_pics") },
+                { "bad_pics",  Path.Combine(directory, "bad_pics") },
+            };
+            foreach (var dir in dirs)
+            {
+                if (Directory.Exists(dir.Value))
+                    Directory.Delete(dir.Value, true);
+                Directory.CreateDirectory(dir.Value);
+            }
             string Way_out_pic = null;
-            if (Directory.Exists(newdirectory))
-                Directory.Delete(newdirectory, true);
-            if (Directory.Exists(picdirectory))
-                Directory.Delete(picdirectory, true);
-            if (Directory.Exists(picdirectorygood))
-                Directory.Delete(picdirectorygood, true);
-            if (Directory.Exists(picdirectorybad))
-                Directory.Delete(picdirectorybad, true);
-            Directory.CreateDirectory(newdirectory);
-            //Directory.CreateDirectory(picdirectory);
-            Directory.CreateDirectory(picdirectorygood);
-            Directory.CreateDirectory(picdirectorybad);
+            int randomSeed = 0;
             Random rnd = new Random();
             // set parameters
             int w = 10000;
             int h = 10000;
             int maxIter = 350;
             double zoom = 0.505;
-            
+
             // other values
             int randw = 0;
             int randh = 0;
@@ -286,35 +288,33 @@ namespace galgen
             Stopwatch st = new Stopwatch();
             st.Start();
 
-            List<List<Tuple<int, int>>> maps = new List<List<Tuple<int, int>>>();
-            List<Tuple<int, int, int, int, double, double>> log = new List<Tuple<int, int, int, int, double, double>>();
+            List<List<Point>> maps = new List<List<Point>>();
+            List<Log> log = new List<Log>();
             int startvalue = 1600;
-            //Parallel.For(startvalue, startvalue + 21, index =>
-            for (int index = startvalue; index < startvalue + 21; index++)
+            Parallel.For(startvalue, startvalue + 21, index =>
+            //for (int index = startvalue; index < startvalue + 21; index++)
             {
-                // generate julia set
-                startofcycle:
-                getvaluesforjuliaset(w, h, rnd, ref randh, ref randw, ref c);
-                List<Tuple<int, int>> vect = GenJulSet(w, h, maxIter, zoom, rnd, randh, randw, c);
-                if (vect.Count > 3000 || vect.Count < 50)
-                    goto startofcycle;
+                List<Point> vect = new List<Point>();
+                do
+                {
+                    vect = GenJulSet(w, h, maxIter, zoom, randomSeed);
+                } while (vect.Count > 3000 || vect.Count < 50);
 
                 maps.Add(vect);
-                
-                log.Add(new Tuple<int, int, int, int, double, double>((maps.Count - 1), 0, randh, randw, c.Real, c.Imaginary));
+                log.Add(new Log((maps.Count - 1), 0, randh, randw, c.Real, c.Imaginary));
                 Console.WriteLine(index);
-            }//);
+            });
             Console.WriteLine(st.Elapsed);
             Console.WriteLine(maps.Count);
-            foreach (List<Tuple<int, int>> vect in maps)
+            foreach (List<Point> vect in maps)
             {
                 // file name
                 string filename = "Insane Julia Set Rand NS " + (maps.IndexOf(vect) + startvalue);
-                
+
                 // export to console
                 //exporttoconsole(vect);
 
-                Way_out_pic = Path.Combine(picdirectorybad, filename + ".jpg");
+                Way_out_pic = Path.Combine(dirs["bad_pics"], filename + ".jpg");
 
                 //if (Console.ReadKey().Key == ConsoleKey.Enter)
                 //{
@@ -331,11 +331,11 @@ namespace galgen
                 //Console.Clear();
 
                 // вывод в файл
-                exporttofile(vect, w, h, maxIter, zoom, rnd, log[maps.IndexOf(vect)], filename, Path.Combine(newdirectory, filename + ".txt"));
+                exporttofile(vect, w, h, maxIter, zoom, rnd, log[maps.IndexOf(vect)], filename, Path.Combine(dirs["map_new"], filename + ".txt"));
 
                 // export to pic
                 exporttopic(vect, Way_out_pic);
-                
+
             }
             //using (StreamWriter sw = new StreamWriter(Path.Combine(directory, "log.txt")))
             //{
@@ -344,6 +344,33 @@ namespace galgen
             //}
             st.Stop();
             Console.WriteLine(st.Elapsed);
+        }
+    }
+
+    public struct Point
+    {
+        public int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public struct Log
+    {
+        public double c1, c2;
+        public int imageId, isGood, randh, randw;
+
+        public Log(int imageId, int isGood, int randh, int randw, double c1, double c2)
+        {
+            this.imageId = imageId;
+            this.isGood = isGood;
+            this.randh = randh;
+            this.randw = randw;
+            this.c1 = c1;
+            this.c2 = c2;
         }
     }
 }
